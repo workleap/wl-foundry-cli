94416c68de26b7338e03cce4d9613dd2c2fb4d03
Alexandre Beaudoin <alexandre.beaudoin@gsoft.com>
2023-04-18 17:34:34 -0400

    More Alex's changes

--- packages/create-workleap/src/bin.ts
+++ packages/create-workleap/src/bin.ts
@@ -5,7 +5,7 @@ import color from "picocolors";
 
 import { name as projectName } from "../package.json";
 
-import { AvailableTemplates, TemplatesIds, generateProject } from "./generateProject";
+import { AvailableTemplates, type TemplatesIds, generateProject } from "./generateProject.js";
 
 const DefaultOutputDirectory = "./my-new-project";
 const NameParameterPosition = 2; // TODO validate position of parameter once ask with `pnpm create`
@@ -23,57 +23,53 @@ const templates: Option<TemplatesIds>[] = AvailableTemplates.map(x => {
     return { value: x, label: x };
 });
 
-async function main() {
-    p.intro(projectName);
+p.intro(projectName);
 
-    if (outputDirectoryFromArgument) {
-        p.note(`${outputDirectoryFromArgument} project setup`);
-    }
+if (outputDirectoryFromArgument) {
+    p.note(`${outputDirectoryFromArgument} project setup`);
+}
 
-    // Ask for output directory
-    const outputDir = (
-        outputDirectoryFromArgument ??
+// Ask for output directory
+const outputDir = (
+    outputDirectoryFromArgument ??
         (await p.text({
             message: "Where should we create the project?",
             placeholder: DefaultOutputDirectory,
             initialValue: DefaultOutputDirectory
         }))
-    );
+);
 
-    if (p.isCancel(outputDir)) {
-        p.cancel(DefaultCancelMessage);
-        process.exit(0);
-    }
-
-    // Ask for other arguments
-    const args = await p.group(
-        {
-            templateId: () => p.select<Option<TemplatesIds>[], TemplatesIds>({
-                message: "Select the template to create",
-                options: templates
-            }),
-            scope: () => p.text({
-                message: "What should be the scope?",
-                placeholder: "Press enter if no scope is needed."
-            })
-        },
-        {
-            onCancel: () => {
-                p.cancel(DefaultCancelMessage);
-                process.exit(0);
-            }
-        }
-    );
-
-    // Call generateProject
-    const loader = p.spinner();
-    loader.start("Generating...");
-
-    await generateProject(outputDir, args);
-
-    loader.stop("Generated!");
-
-    p.outro(color.green("Done!"));
+if (p.isCancel(outputDir)) {
+    p.cancel(DefaultCancelMessage);
+    process.exit(0);
 }
 
-main().catch(console.error);
+// Ask for other arguments
+const args = await p.group(
+    {
+        templateId: () => p.select<Option<TemplatesIds>[], TemplatesIds>({
+            message: "Select the template to create",
+            options: templates
+        }),
+        scope: () => p.text({
+            message: "What should be the scope?",
+            placeholder: "Press enter if no scope is needed."
+        })
+    },
+    {
+        onCancel: () => {
+            p.cancel(DefaultCancelMessage);
+            process.exit(0);
+        }
+    }
+);
+
+// Call generateProject
+const loader = p.spinner();
+loader.start("Generating...");
+
+await generateProject(outputDir, args);
+
+loader.stop("Generated!");
+
+p.outro(color.green("Done!"));
