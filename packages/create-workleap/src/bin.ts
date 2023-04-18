a90de3670311cdf392d281be87afc843a23f5914
Alexandre Beaudoin <alexandre.beaudoin@gsoft.com>
2023-04-18 16:10:32 -0400

    New project format almost done

--- packages/create-workleap/src/bin.ts
+++ packages/create-workleap/src/bin.ts
@@ -1,111 +1,84 @@
 #!/usr/bin/env node
-import * as child_process from "child_process";
 import process from "process";
-import { spinner } from "@clack/prompts";
+import * as p from "@clack/prompts";
+import color from "picocolors";
 
-import { intro, select, note, text, outro } from "./prompts";
-import * as pkg from "../package.json";
+import { name as projectName } from "../package.json";
+
+import { TemplatesIds, generateProject } from "./generateProject";
 
 const DefaultOutputDirectory = "./my-new-project";
 const NameParameterPosition = 2; // TODO validate position of parameter once ask with `pnpm create`
+const DefaultCancelMessage = "Operation cancelled.";
 
-const FoundryCmd = "foundry";
+const outputDirectoryFromArgument: string = process.argv[NameParameterPosition];
 
-const askForOutputDirectoryAsync = async (): Promise<string> => {
-    const outputDirectoryFromArgument: string =
-    process.argv[NameParameterPosition];
+const templates = [
+    { value: "host-application", label: "host-application" },
+    { value: "remote-module", label: "remote-module" },
+    { value: "static-module", label: "static-module" }
+];
+
+async function main() {
+    p.intro(projectName);
 
     if (outputDirectoryFromArgument) {
-        note(`${outputDirectoryFromArgument} project setup`);
+        p.note(`${outputDirectoryFromArgument} project setup`);
     }
 
-    return (
+    // Ask for output directory
+    const outputDir = (
         outputDirectoryFromArgument ??
-    (await text(
-        "Where should we create the project?",
-        DefaultOutputDirectory,
-        DefaultOutputDirectory
-    ))
+        (await p.text({
+            message: "Where should we create the project?",
+            placeholder: DefaultOutputDirectory,
+            initialValue: DefaultOutputDirectory
+        }))
     );
-};
 
-const askForTemplateAsync = (): Promise<string> => {
-    const templates = [
-        { value: "host-application" },
-        { value: "remote-module" },
-        { value: "static-module" }
-    ];
-
-    return select<string>("Select the template to create", templates);
-};
-
-const askForScopeAsync = (template: string): Promise<string> => {
-    return text(
-        `What should be the ${template} scope?`,
-        "Press enter if no scope is needed."
-    );
-};
-
-const callFoundryAsync = async (
-    outputDirectory: string,
-    template: string,
-    scope: string
-): Promise<number> => {
-    const options = [template];
-
-    if (outputDirectory) {
-        options.push("-o", outputDirectory);
+    if (p.isCancel(outputDir)) {
+        p.cancel(DefaultCancelMessage);
+        process.exit(0);
     }
 
-    if (scope) {
-        if (template === "host-application") {
-            options.push("--package-scope", scope);
-        } else {
-            options.push("--host-scope", scope);
+    // Ask for template
+    const templateId = await p.select({
+        message: "Select the template to create",
+        options: templates
+    });
+
+    if (p.isCancel(templateId)) {
+        p.cancel(DefaultCancelMessage);
+        process.exit(0);
+    }
+
+    // Ask for other arguments
+    const args = await p.group(
+        {
+            scope: () => p.text({
+                message: `What should be the ${templateId} scope?`,
+                placeholder: "Press enter if no scope is needed."
+            })
+        },
+        {
+            // On Cancel callback that wraps the group
+            // So if the user cancels one of the prompts in the group this function will be called
+            onCancel: () => {
+                p.cancel(DefaultCancelMessage);
+                process.exit(0);
+            }
         }
-    }
+    );
 
-    const childProcess = child_process.spawn(FoundryCmd, options, {
-        cwd: process.cwd(),
-        shell: true
-    });
-
-    return new Promise(resolve => {
-        childProcess.stdout.on("data", (x: string): void => {
-            process.stdout.write(x.toString());
-        });
-        childProcess.stderr.on("data", (x: string): void => {
-            process.stderr.write(x.toString());
-            process.exit(1);
-        });
-        childProcess.on("exit", (code?: number): void => {
-            resolve(code ?? 0);
-        });
-    });
-};
-
-const main = async (): Promise<void> => {
-    intro(pkg.name);
-
-    const outputDirectory = await askForOutputDirectoryAsync();
-
-    const template = await askForTemplateAsync();
-
-    const scope = await askForScopeAsync(template);
-
-    const loader = spinner();
+    // Call generateProject
+    const loader = p.spinner();
     loader.start("Generating...");
-    await callFoundryAsync(outputDirectory, template, scope);
+
+    await generateProject(templateId as TemplatesIds, outputDir, args);
+
     loader.stop("Generated!");
 
-    outro("Done!");
-};
+    p.outro(color.green("Done!"));
+}
 
-main()
-    .then(() => {
-        process.exit(0);
-    })
-    .catch(error => {
-        console.error(error);
-        process.exit(1);
-    });
+main().catch(console.error);
---
+++ packages/create-workleap/src/generateProject.ts
@@ -0,0 +1,44 @@
+import child_process from "child_process";
+import process from "process";
+
+const FoundryCmd = "foundry";
+
+export type TemplatesIds = "host-application" | "remote-module" | "static-module";
+
+interface Arguments {
+    scope?: string;
+}
+
+export const generateProject = async (templateId: TemplatesIds, outputDir: string, args: Arguments): Promise<number> => {
+    const options: string[] = [templateId];
+
+    if (outputDir) {
+        options.push("-o", outputDir);
+    }
+
+    if (args.scope) {
+        if (templateId === "host-application") {
+            options.push("--package-scope", args.scope);
+        } else {
+            options.push("--host-scope", args.scope);
+        }
+    }
+
+    const childProcess = child_process.spawn(FoundryCmd, options, {
+        cwd: process.cwd(),
+        shell: true
+    });
+
+    return new Promise(resolve => {
+        childProcess.stdout.on("data", (x: string): void => {
+            process.stdout.write(x.toString());
+        });
+        childProcess.stderr.on("data", (x: string): void => {
+            process.stderr.write(x.toString());
+            process.exit(1);
+        });
+        childProcess.on("exit", (code?: number): void => {
+            resolve(code ?? 0);
+        });
+    });
+};
