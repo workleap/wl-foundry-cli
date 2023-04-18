039c54809acd28882c196c6afcf3dac13a938682
Alexandre Asselin <alexasselin008@gmail.com>
2023-04-18 14:51:22 -0400

    chore: Add Shared Eslint and TypeScript configs (#5)
    * chore: added workflows, vssettings and editor config
    
    * disable changeset from running on those packages for now
    
    * Fix linting
    
    * fix breaking test
    
    * add eslint rule and fix them
    
    * add typescript configs
    
    * fix yarn lock
    
    * fix test

--- packages/foundry/src/templates.ts
+++ packages/foundry/src/templates.ts
@@ -6,85 +6,85 @@ import { addToReplace } from "./generator";
 const BaseRepositoryAddress = "Workleap/wl-foundry-cli/templates";
 
 export interface Options extends OptionValues {
-  outDir: string;
+    outDir: string;
 }
 
 export interface TemplateInterface {
-  description: string;
-  repositoryUrl: string;
-  options: {
-    flag: string;
-    description?: string;
-    defaultValue?: string | boolean | [] | string[] | undefined;
-  }[];
-  action?: (options: Options) => Options;
+    description: string;
+    repositoryUrl: string;
+    options: {
+        flag: string;
+        description?: string;
+        defaultValue?: string | boolean | [] | string[] | undefined;
+    }[];
+    action?: (options: Options) => Options;
 }
 
 const getName = (outputDirectory: string): string => {
-  return path.basename(outputDirectory);
+    return path.basename(outputDirectory);
 };
 
 const getScope = (options: Options, flagName: string): string => {
-  const scope = options[flagName];
+    const scope = options[flagName];
 
-  return scope ? `${scope.toString()}/` : "";
+    return scope ? `${scope.toString()}/` : "";
 };
 
 export const Templates: { [key: string]: TemplateInterface } = {
-  "host-application": {
-    description: "use the host-application template",
-    repositoryUrl: `${BaseRepositoryAddress}/host-application`,
-    options: [
-      {
-        flag: "--package-scope <string>",
-        description: "package scope",
-      },
-    ],
-    action: (options) => {
-      const scope = getScope(options, "packageScope");
-      const name = getName(options.outDir);
+    "host-application": {
+        description: "use the host-application template",
+        repositoryUrl: `${BaseRepositoryAddress}/host-application`,
+        options: [
+            {
+                flag: "--package-scope <string>",
+                description: "package scope"
+            }
+        ],
+        action: options => {
+            const scope = getScope(options, "packageScope");
+            const name = getName(options.outDir);
 
-      addToReplace("**/package.json", { PACKAGE_SCOPE: scope, NAME: name });
-      addToReplace("**/@apps/host", { PACKAGE_SCOPE: scope, NAME: name });
-      addToReplace("README.md", { PACKAGE_SCOPE: scope, NAME: name });
+            addToReplace("**/package.json", { PACKAGE_SCOPE: scope, NAME: name });
+            addToReplace("**/@apps/host", { PACKAGE_SCOPE: scope, NAME: name });
+            addToReplace("README.md", { PACKAGE_SCOPE: scope, NAME: name });
 
-      return options;
+            return options;
+        }
     },
-  },
-  "remote-module": {
-    description: "use the remote-module template",
-    repositoryUrl: `${BaseRepositoryAddress}/remote-module`,
-    options: [
-      {
-        flag: "--host-scope <string>",
-        description: "host scope",
-      },
-    ],
-    action: (options) => {
-      const scope = getScope(options, "hostScope");
-      const name = getName(options.outDir);
+    "remote-module": {
+        description: "use the remote-module template",
+        repositoryUrl: `${BaseRepositoryAddress}/remote-module`,
+        options: [
+            {
+                flag: "--host-scope <string>",
+                description: "host scope"
+            }
+        ],
+        action: options => {
+            const scope = getScope(options, "hostScope");
+            const name = getName(options.outDir);
 
-      addToReplace("**", { HOST_SCOPE: scope, NAME: name });
+            addToReplace("**", { HOST_SCOPE: scope, NAME: name });
 
-      return options;
+            return options;
+        }
     },
-  },
-  "static-module": {
-    description: "use the static-module template",
-    repositoryUrl: `${BaseRepositoryAddress}/static-module`,
-    options: [
-      {
-        flag: "--host-scope <string>",
-        description: "host scope",
-      },
-    ],
-    action: (options) => {
-      const scope = getScope(options, "hostScope");
-      const name = getName(options.outDir);
+    "static-module": {
+        description: "use the static-module template",
+        repositoryUrl: `${BaseRepositoryAddress}/static-module`,
+        options: [
+            {
+                flag: "--host-scope <string>",
+                description: "host scope"
+            }
+        ],
+        action: options => {
+            const scope = getScope(options, "hostScope");
+            const name = getName(options.outDir);
 
-      addToReplace("**", { HOST_SCOPE: scope, NAME: name });
+            addToReplace("**", { HOST_SCOPE: scope, NAME: name });
 
-      return options;
-    },
-  },
+            return options;
+        }
+    }
 };
