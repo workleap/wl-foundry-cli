1e773ab0f242d6502575408ad324a72e3c7482c7
Alexandre Beaudoin <abeaudoin@coldmechanics.com>
2023-04-19 20:33:20 -0400

    Add tests and fix some replaceToken method call

--- packages/foundry/src/replaceTokens.ts
+++ packages/foundry/src/replaceTokens.ts
@@ -1,20 +1,19 @@
-import { readFile } from "node:fs/promises";
 import path from "node:path";
 import fse from "fs-extra";
 import { glob } from "glob";
 import handlebars from "handlebars";
 
-export async function replaceTokens(files: string[], values: { [key:string]: any }, outputDir: string): Promise<void> {
+export async function replaceTokens(files: string[], values: { [key:string]: unknown }, outputDir: string): Promise<void> {
     if (!files || files.length === 0 || !values || Object.keys(values).length === 0) {
         return;
     }
 
-    const filesToReplace = glob(files, { ignore: "node_modules/**", cwd: outputDir });
+    const filesToReplace = await glob(files, { ignore: "node_modules/**", cwd: outputDir });
 
     for (const fileToReplace in filesToReplace) {
         const fileToReplacePath = path.join(outputDir, fileToReplace);
 
-        const content: Buffer = await readFile(fileToReplacePath);
+        const content: Buffer = await fse.readFile(fileToReplacePath);
 
         const template = handlebars.compile(content.toString());
 
--- packages/foundry/tests/replaceTokens.test.ts
+++ packages/foundry/tests/replaceTokens.test.ts
@@ -1,11 +1,70 @@
+import * as path from "node:path";
+import * as fse from "fs-extra";
+import * as glob from "glob";
+import * as handlebars from "handlebars";
+
+import { replaceTokens } from "../src/replaceTokens";
+
+jest.mock("node:fs/promises");
+jest.mock("node:path");
+jest.mock("fs-extra");
+jest.mock("glob");
+jest.mock("handlebars");
+
+const fakeFilePatternList = ["foo.bar", "hello/**"];
+
+const fakeFileList = ["foo.bar", "hello/world.txt", "hello/mr/anderson.txt"];
+
+const fakeReplaceValueList = { "FOO": "bar", "PING": "pong" };
+
 describe("replaceTokens", () => {
     afterEach(() => {
         jest.restoreAllMocks();
     });
 
-    test("When Then", async () => {
-        await Promise.resolve();
+    test("When no file Then do nothing", async () => {
+        await replaceTokens([], fakeReplaceValueList, ".");
 
-        expect(true).toBeTruthy();
+        expect(path.join).not.toHaveBeenCalled();
+        expect(glob.glob).not.toHaveBeenCalled();
+        expect(fse.readFile).not.toHaveBeenCalled();
+        expect(fse.writeFile).not.toHaveBeenCalled();
+        expect(handlebars.compile).not.toHaveBeenCalled();
+    });
+
+    test("When no value to replace Then do nothing", async () => {
+        await replaceTokens(fakeFilePatternList, {}, ".");
+
+        expect(path.join).not.toHaveBeenCalled();
+        expect(glob.glob).not.toHaveBeenCalled();
+        expect(fse.readFile).not.toHaveBeenCalled();
+        expect(fse.writeFile).not.toHaveBeenCalled();
+        expect(handlebars.compile).not.toHaveBeenCalled();
+    });
+
+    test("When pattern match no file Then do no file system io", async () => {
+        jest.spyOn(glob, "glob").mockImplementation(async () => []);
+
+        await replaceTokens(fakeFilePatternList, fakeReplaceValueList, ".");
+
+        expect(glob.glob).toHaveBeenCalled();
+        expect(path.join).not.toHaveBeenCalled();
+        expect(fse.readFile).not.toHaveBeenCalled();
+        expect(fse.writeFile).not.toHaveBeenCalled();
+        expect(handlebars.compile).not.toHaveBeenCalled();
+    });
+
+    test("When patter match files Then work", async () => {
+        jest.spyOn(glob, "glob").mockImplementation(async () => fakeFileList);
+        jest.spyOn(fse, "readFile").mockImplementation(async () => "");
+        jest.spyOn(handlebars, "compile").mockImplementation(() => jest.fn());
+
+        await replaceTokens(fakeFilePatternList, fakeReplaceValueList, ".");
+
+        expect(glob.glob).toHaveBeenCalled();
+        expect(path.join).toHaveBeenCalled();
+        expect(fse.readFile).toHaveBeenCalled();
+        expect(fse.writeFile).toHaveBeenCalled();
+        expect(handlebars.compile).toHaveBeenCalled();
     });
 });
