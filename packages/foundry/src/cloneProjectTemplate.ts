34a354ed6f9dc56f9fd00b96a76ad63ef3feb41a
Alexandre Asselin <alexandre.asselin@gsoft.com>
2023-04-25 09:44:44 -0400

    push small synthax change + pnpm lock

--- packages/foundry/src/cloneProjectTemplate.ts
+++ packages/foundry/src/cloneProjectTemplate.ts
@@ -4,6 +4,5 @@ import { ensureDir } from "fs-extra";
 export async function cloneProjectTemplate (outputDirectory: string, repositoryUrl: string) {
     await ensureDir(outputDirectory);
 
-    const runner = degit(repositoryUrl);
-    await runner.clone(outputDirectory);
+    await degit(repositoryUrl).clone(outputDirectory);
 }
