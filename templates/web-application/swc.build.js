5109ac845ae28b67d89bee4e7fba271a27c0a0f5
Alexandre Beaudoin <alexandre.beaudoin@gsoft.com>
2023-05-12 13:56:48 -0400

    Remove ignoreDynamic from swc config (#39)
    * Remove ignoreDynamic from swc config
    
    Also:
    - Update @workleap/eslint-plugin to 1.5.0 with the fixed rules
    - Add msw section to package.json
    
    * Update lockfile and remove duplicated EsLint rule

--- templates/web-application/swc.build.js
+++ templates/web-application/swc.build.js
@@ -32,8 +32,6 @@ export default {
         // The output module resolution system that the code will be compiled for.
         type: "es6",
         // Prevent SWC from exporting the `__esModule` property.
-        strict: true,
-        // Preserve dynamic imports.
-        ignoreDynamic: true
+        strict: true
     }
 };
