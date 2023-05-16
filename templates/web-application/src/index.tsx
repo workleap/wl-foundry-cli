ccb56a17b07835cd5da74f0686637ec9a190d665
Alexandre Beaudoin <alexandre.beaudoin@gsoft.com>
2023-05-16 14:15:17 -0400

    MSW is now optionnal and can be activate from `dev:mock` command ;; WLFE-64 (#41)
    * MSW is now optionnal and require dev:mock command

--- templates/web-application/src/index.tsx
+++ templates/web-application/src/index.tsx
@@ -6,15 +6,19 @@ import { App } from "./App.tsx";
 import { Loading } from "./components/Loading.tsx";
 import { createRoot } from "react-dom/client";
 
-const root = createRoot(document.getElementById("root")!);
-
 // Mock Service Worker
 if (process.env.NODE_ENV === "development") {
-    import("./mocks/browser.ts").then(({ worker }) => {
-        worker.start();
-    });
+    // MOCK value comes from the webpack.DefinePlugin
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    if (MOCK ?? false) {
+        import("./mocks/browser.ts").then(({ worker }) => {
+            worker.start();
+        });
+    }
 }
 
+const root = createRoot(document.getElementById("root")!);
 root.render(
     <StrictMode>
         <Suspense fallback={<Loading />}>
