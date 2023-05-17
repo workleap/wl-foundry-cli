cdb5f24a3c041b5b00f4b7291689003215912232
Alexandre Asselin <alexandre.asselin@gsoft.com>
2023-05-17 09:59:40 -0400

    add support for SVGR in storybook

--- templates/web-application/.storybook/main.ts
+++ templates/web-application/.storybook/main.ts
@@ -1,4 +1,5 @@
 import type { StorybookConfig } from "@storybook/react-webpack5";
+import type  { Configuration } from "webpack";
 
 const config: StorybookConfig = {
   stories: [
@@ -15,10 +16,40 @@ const config: StorybookConfig = {
     name: "@storybook/react-webpack5",
     options: {},
   },
+  webpackFinal: (config) => {
+    configureSvgrWebpack(config);
+
+    return config;
+  },
   docs: {
     autodocs: "tag",
   },
   staticDirs: ["../public"],
 };
+
+function configureSvgrWebpack(config: Configuration) {
+    if(!config.module?.rules) return config;
+
+    // this modifies the existing image rule to exclude .svg files
+    // since we want to handle those files with @svgr/webpack
+    config.module.rules.forEach(rule => {
+        if(
+            typeof rule !== 'string' &&
+            rule.test instanceof RegExp &&
+            rule.test.test('.svg')) {
+                rule.exclude = /\.svg$/i;
+        }
+      })
+
+    // Configure .svg files to be loaded with @svgr/webpack
+    config.module?.rules?.push({
+        test: /\.svg$/,
+        issuer: /\.(ts|tsx)$/i,
+        use: ['@svgr/webpack'],
+    });
+
+    return config;
+}
+
 
 export default config;
