68f094f20c9b67d7e22249fc6eaf75dcb7acc53d
Alexandre Asselin <alexandre.asselin@gsoft.com>
2023-05-17 17:21:42 -0400

    review fixes

--- templates/web-application/src/components/Loading.stories.ts
+++ templates/web-application/src/components/Loading.stories.ts
@@ -12,7 +12,7 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 // More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
-export const Primary: Story = {
+export const Default: Story = {
     args: {
         message: "Loading..."
     }
