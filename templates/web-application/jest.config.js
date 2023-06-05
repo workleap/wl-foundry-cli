013016815d58bad407d034b8dcc6879cf8b4fd0a
Alexandre Beaudoin <alexandre.beaudoin@gsoft.com>
2023-06-05 10:13:46 -0400

    Fix (remove) the failing test, update dependencies and add test-ci script (#69)
    * Fix (remove) the failing test, update dependencies and add test-ci script
    
    * Update the project PNPM lockfile after template update
    
    * Make Jest-Junit reporter on CI build only

--- templates/web-application/jest.config.js
+++ templates/web-application/jest.config.js
@@ -1,10 +1,25 @@
 import swcConfig from "./swc.dev.js";
 
-export default {
+const config = {
     transform: {
         "^.+\\.(t|j)sx?$": ["@swc/jest", swcConfig]
     },
     testEnvironment: "jsdom",
     testPathIgnorePatterns: ["/node_modules/", "/dist/"],
-    extensionsToTreatAsEsm: [".ts", ".tsx"]
+    extensionsToTreatAsEsm: [".ts", ".tsx"],
+    reporters: [["default", {}]]
 };
+
+if (process.argv.includes("--ci")) {
+    config.reporters.push(["jest-junit", {
+        outputDirectory: "reports",
+        outputName: "jest-junit.xml",
+        ancestorSeparator: " › ",
+        uniqueOutputName: "false",
+        suiteNameTemplate: "{filepath}",
+        classNameTemplate: "{classname}",
+        titleTemplate: "{title}"
+    }]);
+}
+
+export default config;
